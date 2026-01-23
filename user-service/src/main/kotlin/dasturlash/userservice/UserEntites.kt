c2afbaf6671fd8fbfcc1e37b67a8cfa5fd833975
saidovdiyorbek <saidovdiyer777@gmail.com>
2026-01-24 01:52:50 +0500

    feat(User): enumerated

--- user-service/src/main/kotlin/dasturlash/userservice/UserEntites.kt
+++ user-service/src/main/kotlin/dasturlash/userservice/UserEntites.kt
@@ -3,9 +3,13 @@ package dasturlash.userservice
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EntityListeners
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.MappedSuperclass
 import jakarta.persistence.Table
 import jakarta.persistence.Temporal
