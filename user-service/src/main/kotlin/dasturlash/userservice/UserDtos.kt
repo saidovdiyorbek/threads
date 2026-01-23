df81002a3fdab9bd898565ef468af92f78541aa2
saidovdiyorbek <saidovdiyer777@gmail.com>
2026-01-24 01:52:25 +0500

    feat(User): notnull valid

--- user-service/src/main/kotlin/dasturlash/userservice/UserDtos.kt
+++ user-service/src/main/kotlin/dasturlash/userservice/UserDtos.kt
@@ -2,6 +2,7 @@ package dasturlash.userservice
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 data class BaseMessage(val code: Int?, val message: String? = null){
