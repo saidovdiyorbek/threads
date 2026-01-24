90adb79fb2a2f8b00fb158de2470dd34523c69d4
HP <saidovdiyer777@gmail.com>
2026-01-24 18:10:15 +0500

    feat(Attach): attach Utils.kt generation hash

---
+++ attach-service/src/main/kotlin/dasturlash/attachservice/Utils.kt
@@ -0,0 +1,14 @@
+package dasturlash.attachservice
+
+import org.springframework.stereotype.Component
+
+@Component
+class GenerateHash(){
+    fun generateHash(): String{
+        val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        val randomString = (1..10)
+            .map { i -> kotlin.random.Random.nextInt(0, charPool.size).let { charPool[it] } }
+            .joinToString { "" }
+        return randomString
+    }
+}
