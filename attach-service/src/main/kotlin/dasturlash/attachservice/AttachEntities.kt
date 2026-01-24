862b508b2055396e26956d89498433760a6e8b3d
HP <saidovdiyer777@gmail.com>
2026-01-24 15:42:45 +0500

    feat(Attach): add base entity and attach entity

---
+++ attach-service/src/main/kotlin/dasturlash/attachservice/AttachEntities.kt
@@ -0,0 +1,39 @@
+package dasturlash.attachservice
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.Temporal
+import jakarta.persistence.TemporalType
+import org.hibernate.annotations.ColumnDefault
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.util.Date
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+class BaseEntity(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null,
+    @CreatedDate @Temporal(TemporalType.TIMESTAMP)var createdDate: Date? = null,
+    @LastModifiedDate @Temporal(TemporalType.TIMESTAMP)var lastModifiedDate: Date? = null,
+    @CreatedBy var createdBy: String? = null,
+    @LastModifiedBy var lastModifiedBy: String? = null,
+    @Column(nullable = false) @ColumnDefault(value = "false") var deleted: Boolean = false,
+)
+
+@Entity
+class Attach(
+    @Column(nullable = false, name = "origin_name") var originName: String? = null,
+    @Column(nullable = false) var size: Long,
+    @Column(nullable = false) var type: String,
+    @Column(nullable = false) var path: String,
+    @Column(nullable = false) var fullPath: String,
+    @Column(nullable = false) var hash: String,
+) : BaseEntity()
