d0c9352f0a78c8bc0ce205e5cceba923437edf6c
saidovdiyorbek <saidovdiyer777@gmail.com>
2026-01-24 12:02:35 +0500

    feat(User): modifiying

--- user-service/src/main/kotlin/dasturlash/userservice/UserRepositories.kt
+++ user-service/src/main/kotlin/dasturlash/userservice/UserRepositories.kt
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.jpa.repository.support.JpaEntityInformation
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository
