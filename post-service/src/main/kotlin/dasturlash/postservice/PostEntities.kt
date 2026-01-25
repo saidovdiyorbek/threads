109ce7739808c9cb44994568b9fdbe3b0fd7bb05
HP <saidovdiyer777@gmail.com>
2026-01-25 21:58:34 +0500

    feat(Post): Post and PostAttach entity

--- post-service/src/main/kotlin/dasturlash/postservice/PostEntities.kt
+++ post-service/src/main/kotlin/dasturlash/postservice/PostEntities.kt
@@ -1,10 +1,16 @@
 package dasturlash.postservice
 
+import jakarta.persistence.CollectionTable
 import jakarta.persistence.Column
+import jakarta.persistence.ElementCollection
+import jakarta.persistence.Entity
 import jakarta.persistence.EntityListeners
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.MappedSuperclass
 import jakarta.persistence.Temporal
 import jakarta.persistence.TemporalType
@@ -26,3 +32,19 @@ class BaseEntity(
     @LastModifiedBy var lastModifiedBy: String? = null,
     @Column(nullable = false) @ColumnDefault(value = "false") var deleted: Boolean = false,
 )
+
+@Entity
+class Post(
+    var text: String? = null,
+    @ManyToOne(fetch = FetchType.LAZY)
+    var post: Post? = null,
+    var userId: Long,
+) : BaseEntity()
+
+@Entity
+class PostAttach(
+    @Column(nullable = false)
+    val attachHash: String,
+    @ManyToOne(fetch = FetchType.LAZY)
+    val post: Post,
+) : BaseEntity()
