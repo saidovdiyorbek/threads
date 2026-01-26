1e171361cc6b351a40388f9aa3ff99bef3d6ce73
HP <saidovdiyer777@gmail.com>
2026-01-26 16:39:50 +0500

    feat(Post): Enable feign JpaRepo JpaAuditing

--- post-service/src/main/kotlin/dasturlash/postservice/PostServiceApplication.kt
+++ post-service/src/main/kotlin/dasturlash/postservice/PostServiceApplication.kt
@@ -2,7 +2,13 @@ package dasturlash.postservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@EnableFeignClients
+@EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl::class)
+@EnableJpaAuditing
 @SpringBootApplication
 class PostServiceApplication
 
