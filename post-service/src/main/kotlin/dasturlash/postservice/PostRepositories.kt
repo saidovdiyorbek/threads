5d4cc202b42164280ed1b5b85f2c8c3a9123189a
HP <saidovdiyer777@gmail.com>
2026-01-26 16:39:18 +0500

    feat(Post): get post attach and remove by hash

--- post-service/src/main/kotlin/dasturlash/postservice/PostRepositories.kt
+++ post-service/src/main/kotlin/dasturlash/postservice/PostRepositories.kt
@@ -7,10 +7,13 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.jpa.repository.support.JpaEntityInformation
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository
 import org.springframework.data.repository.NoRepositoryBean
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Repository
 
 @NoRepositoryBean
 interface BaseRepository<T : BaseEntity> : JpaRepository<T, Long>, JpaSpecificationExecutor<T> {
@@ -42,4 +45,24 @@ class BaseRepositoryImpl<T : BaseEntity>(
 
     override fun findAllNotDeleted(pageable: Pageable): Page<T> = findAll(isNotDeletedSpecification, pageable)
 
+}
+
+interface PostRepository : BaseRepository<Post>{
+
+}
+@Repository
+interface PostAttachRepository : BaseRepository<PostAttach>{
+
+    @Query("""
+        select pa.attachHash from PostAttach pa
+        where pa.post.id = ?1 and pa.deleted = false
+    """)
+    fun getPostAttachHash(postId: Long): List<String>
+
+    @Modifying
+    @Query("""
+        delete from PostAttach pa
+        where pa.attachHash in ?1
+    """)
+    fun removeByHashesList(hashes: List<String>)
 }
