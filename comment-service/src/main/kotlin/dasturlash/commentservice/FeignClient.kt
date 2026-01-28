7e0bb0ded9c174715883b80c784b1d2496c47182
HP <saidovdiyer777@gmail.com>
2026-01-28 14:28:53 +0500

    feat(Comment): user, post, attach client

--- comment-service/src/main/kotlin/dasturlash/commentservice/FeignClient.kt
+++ comment-service/src/main/kotlin/dasturlash/commentservice/FeignClient.kt
@@ -1,2 +1,42 @@
 package dasturlash.commentservice
 
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+
+@FeignClient(name = "user-service", url = "\${services.hosts.user}/internal/api/v1/users")
+interface UserClient {
+    @GetMapping("/{id}/exists")
+    fun exists(@PathVariable id: Long): Boolean
+
+    @GetMapping("/{id}/get-user-short-info")
+    fun getUserShortInfo(@PathVariable id: Long): UserShortInfo?
+}
+
+@FeignClient(name = "post-service", url = "\${services.hosts.post}/internal/api/v1/posts")
+interface PostClient {
+    @GetMapping("/{id}/exists")
+    fun exists(@PathVariable id: Long): Boolean
+
+    @PutMapping("/increment-post-comment-count/{id}")
+    fun incrementPostComment(@PathVariable id: Long)
+
+    @PutMapping("/decrement-post-comment-count/{id}")
+    fun decrementPostComment(@PathVariable id: Long)
+}
+
+@FeignClient(name = "attach-service", url = "\${services.hosts.attach}/internal/api/v1/attaches")
+interface AttachClient{
+    @GetMapping("/{hash}/exists")
+    fun exists(@PathVariable hash: String): Boolean
+
+    @PostMapping("/hashes/exists")
+    fun listExists(@RequestBody hashes: List<String>): Boolean
+
+    @DeleteMapping("/deleteList")
+    fun deleteList(@RequestBody hashes: List<String>)
+}
