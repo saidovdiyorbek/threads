f735f6f2427c0c4923ead7bee56b22bbf36088d5
HP <saidovdiyer777@gmail.com>
2026-01-31 22:29:40 +0500

    feat(Attach): add Resource server config

--- attach-service/build.gradle.kts
+++ attach-service/build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 extra["springCloudVersion"] = "2025.0.1"
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
--- attach-service/src/main/kotlin/dasturlash/attachservice/AttachDtos.kt
+++ attach-service/src/main/kotlin/dasturlash/attachservice/AttachDtos.kt
@@ -14,3 +14,10 @@ data class AttachUrl(
     val hash: String,
     val url: String,
 )
+
+data class UserInfoResponse(
+    val id: Long,
+    val fullName: String,
+    val username: String,
+    val role: String,
+)
